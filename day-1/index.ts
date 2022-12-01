db8847edf1d40c25423aac2f61aa793aefb083a5
survikrowa <szymonhrabia60@gmail.com>
2022-12-01 20:17:58 +0100

    day-1-part-2

--- day-1/index.ts
+++ day-1/index.ts
@@ -2,6 +2,7 @@ import { readFileSync } from "node:fs";
 import { join } from "node:path";
 
 const elfsCalories = readFileSync(join(__dirname, "input.txt")).toString();
+//Part 1
 const calories = elfsCalories
   .split("\n\n")
   .map((elf) => elf.split("\n"))
@@ -12,4 +13,15 @@ const calories = elfsCalories
     return Math.max(a, b);
   });
 
-console.log(calories);
+//Part 2
+const [firstElf, secondElf, thirdElf] = elfsCalories
+  .split("\n\n")
+  .map((elf) => elf.split("\n"))
+  .flatMap((elf) => {
+    return elf.reduce((a, b) => a + Number(b), 0);
+  })
+  .sort((a, b) => b - a);
+
+const totalCalories = firstElf + secondElf + thirdElf;
+
+console.log(totalCalories);
